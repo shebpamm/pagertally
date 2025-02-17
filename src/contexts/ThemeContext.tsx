2acf2d6339fc5908fcda0dca565bb22ff819447e
Erik Karsten <erik@opcode.xyz>
2025-02-17 18:44:10 +0200

    init: no colors

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import type React from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import themeData from "../../styles/theme.json";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+  themeColors: typeof themeData.light;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") as Theme;
+    if (savedTheme) {
+      if (savedTheme === "light" || savedTheme === "dark") {
+        setTheme(savedTheme);
+      } else {
+        setTheme("light");
+        localStorage.setItem("theme", "light");
+      }
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  const themeColors = themeData[theme];
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme, themeColors }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
